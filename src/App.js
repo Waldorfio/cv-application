c1153f4a1d5a0771d3d1d65e353d86fb268a444b
Waldorfio <walid.hanifi707@gmail.com>
2022-10-14 18:57:14 +1000

    Refactor to constructor

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+import React, { Component } from 'react'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
 }
 
 export default App;

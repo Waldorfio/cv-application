681011c9efa188d82f23687113220bb2d3debcf0
Waldorfio <walid.hanifi707@gmail.com>
2022-10-15 15:43:24 +1000

    Pass current state into form component

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react"
+import React, { Component } from 'react'
+import Form from './Form'
 
 class GeneralInfo extends Component {
     constructor(props) {
@@ -22,33 +23,39 @@ class GeneralInfo extends Component {
     }
 
     nameChange(e) {
+        e.preventDefault();
         this.setState({
             name: e.target.value,
         })
     }
     titleChange(e) {
+        e.preventDefault();
         this.setState({
             title: e.target.value,
         })
     }
     emailChange(e) {
+        e.preventDefault();
         this.setState({
             email: e.target.value,
         })
     }
     phoneChange(e) {
+        e.preventDefault();
         this.setState({
             phone: e.target.value,
         })
     }
     locationChange(e) {
+        e.preventDefault();
         this.setState({
             location: e.target.value,
         })
     }
     summaryChange(e) {
+        e.preventDefault();
         this.setState({
-            sunmmary: e.target.value,
+            summary: e.target.value,
         })
     }
 
@@ -58,11 +65,12 @@ class GeneralInfo extends Component {
                 <form>
                     <label> Name:<input type="text" id="name" onChange={this.nameChange}></input></label>
                     <label> Title:<input type="text" id="title" onChange={this.titleChange}></input></label>
-                    <label> email:<input type="text" id="email" onChange={this.emailChange}></input></label>
+                    <label> Email:<input type="text" id="email" onChange={this.emailChange}></input></label>
                     <label> Phone:<input type="text" id="phone" onChange={this.phoneChange}></input></label>
                     <label> Location:<input type="text" id="location" onChange={this.locationChange}></input></label>
                     <label> Summary:<input type="text" id="summary" onChange={this.summaryChange}></input></label>
                 </form>
+                < Form general={this.state} />
             </div>
         )
     }
